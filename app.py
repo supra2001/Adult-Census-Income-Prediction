4a174dcd87f7ac859ff80908a4fb0db02bd14e29
Supratim Mukherjee <supratim.2127@gmail.com>
2025-11-12 00:41:43 +0530

    error

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from src.logger import logging
+from src.exception import CustomException
+import os, sys
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET','POST'])
+def index():
+    try:
+        raise Exception("We are testing our second methods of logging.")
+    except Exception as e:
+        abc = CustomException(e, sys)
+        logging.info(abc.error_message)
+        return "The Logging is successful."
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ src/components/data_ingestion.py
@@ -0,0 +1,50 @@
+import os, sys
+import pandas as pd
+import numpy as np
+from logger import logging
+from src.exception import CustomException
+from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path = os.path.join("artifact/data_ingestion", "train.csv")
+    test_data_path = os.path.join("artifact/data_ingestion", "test.csv")
+    raw_data_path = os.path.join("artifact/data_ingestion", "raw.csv")
+    
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+        
+    def initiate_data_ingestion(self):
+        logging.info("Data Ingestion started")
+        try:
+            logging.info("Data Reading using Pandas library from local system")
+            data = pd.read_csv(os.path.join("notebook/data", "income_cleandata.csv"))
+            logging.info("Data Reading Completed")
+            
+            os.makedirs(os.path.dirname(self.ingestion_config.raw_data_path), exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path, index=False)
+            logging.info("Data splitted into train and test")
+            
+            train_set , test_set =train_test_split(data, test_size=0.30, random_state=42)
+            
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            
+            logging.info("Data Ingestion Completed")
+            
+            return (
+                self.ingestion_config.test_data_path,
+                self.ingestion_config.test_data_path
+            )
+            
+        except Exception as e:
+            logging.info("Error occured in data ingestion phase")
+            raise CustomException(e, sys)
+    
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
