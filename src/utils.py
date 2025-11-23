04ede2e47f1b08ed2a05a46cf3569469c68fc4bf
Supratim Mukherjee <supratim.2127@gmail.com>
2025-11-23 22:22:10 +0530

    Model Training Complete

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,8 @@ from src.logger import logging
 from src.exception import CustomException
 import os, sys
 import pickle
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_recall_curve, f1_score, precision_score, recall_score
 
 def save_object(file_path, obj):
     try:
@@ -11,5 +13,44 @@ def save_object(file_path, obj):
         with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
             
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)    
+    
+    
+def evaluate_model(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+
+            # Correct variable
+            model = list(models.values())[i]
+
+            # Correct param mapping
+            param = params[list(models.keys())[i]]
+            
+            GS = GridSearchCV(model, param, cv=5)
+            GS.fit(X_train, y_train)
+            
+            model.set_params(**GS.best_params_)
+            model.fit(X_train, y_train)
+
+            # Predictions
+            y_pred = model.predict(X_test)
+            test_model_accuracy = accuracy_score(y_test, y_pred)
+
+            # Store using model name, not model object
+            report[list(models.keys())[i]] = test_model_accuracy
+
+        return report  # <-- moved outside loop
+    
     except Exception as e:
         raise CustomException(e, sys)
